576caa0df606c18f380a8aa5b4ac6768a6d045b5
pavanaachar <pavana.s.achar1991@gmail.com>
2017-03-16 02:27:21 -0700

    sample java directory explorer

---
+++ DirExplorer.java
@@ -0,0 +1,16 @@
+package text;
+import java.io.File;
+
+
+public class DirExplorer {
+	File directory;
+	public DirExplorer(File file){
+		this.directory=file;
+	}
+	
+	public File[] listfiles(){
+		File[] files = directory.listFiles();
+		return files;
+	}
+}
+
