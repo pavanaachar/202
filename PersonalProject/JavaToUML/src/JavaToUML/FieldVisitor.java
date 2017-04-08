9328176f6194b7d375868995f79f067b74e5e7c8
pavanaachar <pavana.s.achar1991@gmail.com>
2017-04-07 18:53:45 -0700

    fetching visibility of the attributes

--- PersonalProject/JavaToUML/src/JavaToUML/FieldVisitor.java
+++ PersonalProject/JavaToUML/src/JavaToUML/FieldVisitor.java
@@ -1,36 +1,51 @@
 package JavaToUML;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
+import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.body.VariableDeclarator;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
 
 public class FieldVisitor extends VoidVisitorAdapter<Object>  {
-	
+
 	public ArrayList<String> FieldName;
-	
+
 	public FieldVisitor(){
 		FieldName = null;
+
 	}
-	
 	public void visit(FieldDeclaration n,Object obj){
 		FieldName = new ArrayList<String>();
 		if(n.getVariables().isEmpty()==false){
-			
-			FieldName.add(n.getVariables().get(0).toString());
-			
-			
-			
+			NodeList<VariableDeclarator> fieldlist = n.getVariables();
+			String name = "";
+			for(VariableDeclarator field: fieldlist){	
+				if(n.isPublic()){
+					name = name+"+";
+				}
+				else if(n.isProtected()){
+					name = name+"#";
+				}
+				else if(n.isPrivate()){
+					name = name+"-";
+				}
+				else{
+					name = name+"+";
+				}
+				name = name+field.toString();
+				FieldName.add(name);
+				System.out.println(name);
+			}
+
 		}
 		else{
-		FieldName.add("");
+			FieldName.add("");
 		}
 	}
-	
 	public ArrayList<String> getFieldName(){
 		return FieldName;
 	}
-
 }
-
