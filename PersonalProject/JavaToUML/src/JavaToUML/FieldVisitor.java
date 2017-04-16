fbd2a7fe77d554391afd48ec80a220cc1ca4bdc9
pavanaachar <pavana.s.achar1991@gmail.com>
2017-04-15 18:58:19 -0700

    parsing the source to find multiplicities in associations

--- PersonalProject/JavaToUML/src/JavaToUML/FieldVisitor.java
+++ PersonalProject/JavaToUML/src/JavaToUML/FieldVisitor.java
@@ -1,6 +1,7 @@
 package JavaToUML;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 
 import com.github.javaparser.ast.NodeList;
@@ -14,17 +15,34 @@ public class FieldVisitor extends VoidVisitorAdapter<Object>  {
 	public ArrayList<String> FieldName;
 	public ArrayList<String> types;
 
+	public static final String[] collections = {"Set","List","Collection","ArrayList","LinkedList","HashSet","queue","dequeue"};
+
 	public FieldVisitor(){
 		FieldName = new ArrayList<String>();
 		types = new ArrayList<String>();
 	}
+
 	@Override
 	public void visit(FieldDeclaration n,Object obj){
-		
+		super.visit(n, obj);
 		if(n.getVariables().isEmpty()==false){
 			NodeList<VariableDeclarator> fieldlist = n.getVariables();
-			String name = "";
+			
 			for(VariableDeclarator field: fieldlist){	
+				
+				String name = "";
+				
+				String type = field.getType().toString();
+				for(int i=0;i<collections.length;i++){
+					if(type.contains(collections[i])){
+						types.add("*"+type);
+						System.out.println("field contains :"+collections[i]);
+						break;
+					}
+				}
+				
+				//check if getter or setter methods exist
+
 				if(n.isPublic()){
 					name = name+"+";
 				}
@@ -37,11 +55,14 @@ public class FieldVisitor extends VoidVisitorAdapter<Object>  {
 				else{
 					name = name+"-";
 				}
-				
+
+
 				name = name+field.toString()+":"+field.getType().toString();
 				FieldName.add(name);
 				types.add(field.getType().toString());
-				System.out.println(name);
+
+
+
 			}
 
 		}
@@ -52,7 +73,7 @@ public class FieldVisitor extends VoidVisitorAdapter<Object>  {
 	public ArrayList<String> getFieldName(){
 		return FieldName;
 	}
-	
+
 	public ArrayList<String> getFieldTypes(){
 		return types;
 	}
