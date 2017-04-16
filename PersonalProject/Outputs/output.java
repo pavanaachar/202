dc0d8f4a8d3562721fe3e5cf684b8e6851505d70
pavanaachar <pavana.s.achar1991@gmail.com>
2017-04-16 14:48:11 -0700

    class diagram without showing the fields for an association

--- PersonalProject/Outputs/output.java
+++ PersonalProject/Outputs/output.java
@@ -1,20 +1,18 @@
 @startuml
 class A{
 +a:int
--b:B
 +sampleMethod(s:String):void
 -anotherMethod(x:int,y:int):void
 }
 class B{
--k:ArrayList<A>
 +hello():void
 }
 interface I{
 }
 class J{
-+i:I
 +method():void
 }
+class A--class B
 class B--"*"class A
 interface I..class J
 interface I<|..class A
