31e57c82ae82e3e2daeb944de10897539cf362f4
pavanaachar <pavana.s.achar1991@gmail.com>
2017-04-20 21:57:01 -0700

    parsing dependency from within method declaration

--- PersonalProject/JavaToUML/src/JavaToUML/MethodVisitor.java
+++ PersonalProject/JavaToUML/src/JavaToUML/MethodVisitor.java
@@ -5,17 +5,20 @@ import java.util.EnumSet;
 
 import com.github.javaparser.ast.Modifier;
 import com.github.javaparser.ast.body.MethodDeclaration;
-import com.github.javaparser.ast.type.TypeParameter;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
 
 public class MethodVisitor extends VoidVisitorAdapter<Object> {
+	
 	public ArrayList<String> Methods = new ArrayList<String>(); 
+	public ArrayList<String> types = new ArrayList<String>(); 
 
 	@Override
 	public void visit(MethodDeclaration n, Object obj) {
+		
+		super.visit(n, obj);
 
-		System.out.print("\n" +n.getNameAsString()+"\n");
+		//System.out.print("\n" +n.getNameAsString()+"\n");
 		EnumSet<Modifier> mod = n.getModifiers();
 		
 		String name = "";
@@ -40,6 +43,13 @@ public class MethodVisitor extends VoidVisitorAdapter<Object> {
 			{
 				name += n.getParameters().get(i).getNameAsString() + ":";
 				name += n.getParameters().get(i).getType();
+				String param_type = n.getParameters().get(i).getType().toString();
+				if(Parser.InterfaceNames.contains(param_type)){
+					if(!types.contains(param_type)){
+						types.add(param_type);
+					}
+					
+				}
 				if(i<params-1){
 					name+=",";
 				}
@@ -50,13 +60,16 @@ public class MethodVisitor extends VoidVisitorAdapter<Object> {
 		}
 		name += ":"+ n.getType();
 		
-		
+	
 		Methods.add(name);
 
 	}
 
 	public ArrayList<String> getMethods(){
 		return Methods;
+	}
+	public ArrayList<String> gettypess(){
+		return types;
 	}
 
 }
