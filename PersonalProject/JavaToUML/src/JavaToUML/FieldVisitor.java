17f91561ff9c60b795962518ed9e7fa6f7542733
pavanaachar <pavana.s.achar1991@gmail.com>
2017-04-20 21:51:31 -0700

    setting cardinality in association

--- PersonalProject/JavaToUML/src/JavaToUML/FieldVisitor.java
+++ PersonalProject/JavaToUML/src/JavaToUML/FieldVisitor.java
@@ -2,7 +2,6 @@ package JavaToUML;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
@@ -16,16 +15,20 @@ public class FieldVisitor extends VoidVisitorAdapter<Object>  {
 
 	public ArrayList<String> FieldName;
 	public ArrayList<String> types;
-
-	List<String> collections = new ArrayList<String>(Arrays.asList("ArrayList","List","Hashmap","Set"));
+	public ArrayList<ObjCount> objCountList;
+	
+	List<String> collections = new ArrayList<String>(Arrays.asList("Collection","ArrayList","List","Hashmap","Set"));
 	public FieldVisitor(){
 		FieldName = new ArrayList<String>();
 		types = new ArrayList<String>();
+		
+		objCountList = new ArrayList<ObjCount>();
 	}
 
 	@Override
 	public void visit(FieldDeclaration n,Object obj){
 		super.visit(n, obj);
+
 		if(n.getVariables().isEmpty()==false)
 		{
 			NodeList<VariableDeclarator> fieldlist = n.getVariables();
@@ -34,25 +37,40 @@ public class FieldVisitor extends VoidVisitorAdapter<Object>  {
 				String name = "";
 				String type = field.getType().toString();
 
-				
+
 				if(type.contains("<"))
 				{
-					System.out.println(type.substring(0,type.indexOf("<")));
+					//System.out.println(type.substring(0,type.indexOf("<")));
 					if(collections.contains(type.substring(0,type.indexOf("<"))))
 					{
-						System.out.println(type.substring(type.indexOf("<")+1,type.indexOf(">")));
+						//System.out.println(type.substring(type.indexOf("<")+1,type.indexOf(">")));
 						String Ctype = type.substring(type.indexOf("<")+1,type.indexOf(">"));
 						if(Parser.ClassNames.contains(Ctype)||Parser.InterfaceNames.contains(Ctype))
 						{
-							types.add("*"+Ctype);
-							System.out.println("types : "+types);
+							
+							boolean found = false;
+							//System.out.println("HERE");
+							for (ObjCount o : objCountList) {
+								if (o.className.equals(Ctype) && o.count < ObjCount.MAX_OBJ_COUNT) {
+									o.count += ObjCount.MAX_OBJ_COUNT;
+									found = true;
+								}
+							}
+							if (!found) {
+								ObjCount o = new ObjCount(Ctype);
+								o.count = ObjCount.MAX_OBJ_COUNT;
+								objCountList.add(o);
+							}
+							
+							
 						}
 					}
 				}
 
 
-				//check if getter or setter methods exist
-				else if(Parser.ClassNames.contains(type)==false&&Parser.InterfaceNames.contains(type)==false)
+				//TO-DO check if getter or setter methods exist
+				
+				else if(Parser.ClassNames.contains(type)==false && Parser.InterfaceNames.contains(type)==false)
 				{
 					if(n.isPublic()){
 						name = name+"+";
@@ -69,13 +87,27 @@ public class FieldVisitor extends VoidVisitorAdapter<Object>  {
 
 					name = name+field.toString()+":"+field.getType().toString();
 					FieldName.add(name);
-					System.out.println("field names : "+FieldName);
-
-
+					//System.out.println("field names : "+FieldName);
 				}
-				else{
+				
+				else
+				{
+					boolean found = false;
+					for (Iterator<ObjCount> iterator = objCountList.iterator(); iterator.hasNext();) {
+						ObjCount o = iterator.next();
+						if (o.className.equals(type)) {
+							o.count++;
+							found = true;
+						}
+					}
+					if (!found) {
+						ObjCount o = new ObjCount(type);
+						o.count = 1;
+						objCountList.add(o);
+					}
+					
 					types.add(type);
-					System.out.println("types : "+types);
+					//System.out.println("types : "+types);
 				}
 			}
 		}
@@ -87,4 +119,10 @@ public class FieldVisitor extends VoidVisitorAdapter<Object>  {
 	public ArrayList<String> getFieldTypes(){
 		return types;
 	}
+	
+	// S
+	public ArrayList<ObjCount> getFieldObjCountList(){
+		return objCountList;
+	}
+
 }
