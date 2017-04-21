4bcda1bafb6e051d07b05e79a29aa77e47038f09
pavanaachar <pavana.s.achar1991@gmail.com>
2017-04-20 21:52:08 -0700

    defining an object to store cardinality

---
+++ PersonalProject/JavaToUML/src/JavaToUML/ObjCount.java
@@ -0,0 +1,12 @@
+package JavaToUML;
+
+public class ObjCount {
+	public static final int MAX_OBJ_COUNT = 20000; 
+	
+	public String className;
+	public int count;
+	
+	ObjCount(String s) {
+		this.className = s;
+	}
+}
