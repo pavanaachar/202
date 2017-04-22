21655c0f817fd5d5a24ce06268fe387ce5fc0422
pavanaachar <pavana.s.achar1991@gmail.com>
2017-04-22 05:13:22 -0700

    test file changes for setter and getter

--- temp/A.java
+++ temp/A.java
@@ -2,11 +2,14 @@ class A implements I {
 
 	public int a;
 
-	private B b;
+	private int num;
 	public B a;
 
 		
-	public void sampleMethod(String s){
+	public void getNum(){
+
+	}
+	public void setnum(int n){
 
 	}
 	private void anotherMethod(int x, int y){
