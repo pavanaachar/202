e9b8b75843e6ad7d7d7de75796b81ce9690e3235
pavanaachar <pavana.s.achar1991@gmail.com>
2017-04-26 15:27:01 -0700

    refactoring code - creating FieldAttr objects

--- PersonalProject/JavaToUML/src/JavaToUML/FieldVisitor.java
+++ PersonalProject/JavaToUML/src/JavaToUML/FieldVisitor.java
@@ -1,82 +1,79 @@
 package JavaToUML;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
+import java.util.Optional;
 
 import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.body.FieldDeclaration;
 import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+import com.github.javaparser.ast.type.Type;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
 
 public class FieldVisitor extends VoidVisitorAdapter<Object>  {
 
-	public ArrayList<String> FieldName;
-	public ArrayList<String> rawFieldNames;
-	//public ArrayList<String> types;
-	public ArrayList<ObjCount> objCountList;
-	public HashMap<String, String> varVisiblity;
-	public HashMap<String, String> varToTypeMap;
+	public ArrayList<FieldAttr> fieldAttrs;
 
-	List<String> collections = new ArrayList<String>(Arrays.asList("Collection","ArrayList","List","Hashmap","Set"));
 	public FieldVisitor(){
-		FieldName = new ArrayList<String>();
-		
-		rawFieldNames = new ArrayList<String>();
-		//types = new ArrayList<String>();
-
-		objCountList = new ArrayList<ObjCount>();
-		varVisiblity = new HashMap<String, String>();
-		varToTypeMap = new HashMap<String, String>();
+		fieldAttrs = new ArrayList<FieldAttr>();
 	}
-/**
-	private void VisiblityMod(FieldDeclaration n, VariableDeclarator field, String name)
-	{
-		if(n.isPublic()){
-			name = name+"+";
-		}
-		else if(n.isProtected()){
-			name = name+"#";
-		}
-		else if(n.isPrivate()){
-			name = name+"-";
-		}
-		else{
-			name = name+"-";
+
+	private void AddVarToFieldAttrs(FieldDeclaration n, VariableDeclarator field) {
+		String typeClass = field.getType().getClass().toString();
+		FieldAttr fAttr = new FieldAttr();
+ 
+		fAttr.name = new String(field.toString());
+		fAttr.fullType = new String(field.getType().toString());
+		
+		if (typeClass.contains("ArrayType")) {
+			fAttr.type = new String(field.getType().toString());
+			fAttr.typeClass = new String(typeClass);
+			fAttr.elementType = new String(field.getType().getElementType().toString());
+
+		} else if (typeClass.contains("ClassOrInterfaceType")) {
+			fAttr.type = new String(((ClassOrInterfaceType)(field.getType())).getName().toString());
+			fAttr.typeClass = new String(typeClass);
+
+			Optional<NodeList<Type>> tList = ((ClassOrInterfaceType)(field.getType())).getTypeArguments();
+			if (tList.isPresent()) {
+				fAttr.argTypes = new ArrayList<FieldAttr>();
+				for (Iterator<Type> iterator = tList.get().iterator(); iterator.hasNext();) {
+					Type t = iterator.next();
+					FieldAttr fAttrInt = new FieldAttr();
+					fAttrInt.type = t.getElementType().toString();
+					fAttrInt.typeClass = t.getElementType().getClass().getName();
+					fAttrInt.fullType = t.getElementType().toString();
+					
+					// The fields fAttrInt.name, fAttrInt.argTypes, fAttrInt.elementType
+					// are not used in this case.
+					
+					fAttr.argTypes.add(fAttrInt);
+				}
+			}
+		} else if (typeClass.contains("PrimitiveType")) {
+			fAttr.type = new String(field.getType().toString());
+			fAttr.typeClass = new String(typeClass);
+		} else {
+			// We are not expecting or handling any other type 
+			assert(false);
 		}
 
-		name = name+field.toString()+":"+field.getType().toString();
-	}*/
-
-	private void SetVisiblityForVar(FieldDeclaration n, VariableDeclarator field)
-	{
+		// Set visibility
 		if (n.isPublic()) {
-			varVisiblity.put(field.toString(), "public");
+			fAttr.visiblity = new String("public");
 		}
 		else if (n.isProtected()) {
-			varVisiblity.put(field.toString(), "protected");
-		}
-		else if (n.isPrivate()) {
-			varVisiblity.put(field.toString(), "private");
+			fAttr.visiblity = new String("protected");
 		}
 		else {
-			varVisiblity.put(field.toString(), "private");
-		}	
-	}
-	
-	private void AddVarToTypeEntry(VariableDeclarator field) {
-		varToTypeMap.put(field.toString(), field.getType().toString());
-	}
-	
-	private void AddRawFieldNameToList(String fieldName) {
-		if (!rawFieldNames.contains(fieldName)) {
-			rawFieldNames.add(fieldName);
+			fAttr.visiblity = new String("private");
 		}
+		
+		fieldAttrs.add(fAttr);
 	}
-	
+
 	@Override
 	public void visit(FieldDeclaration n,Object obj){
 		super.visit(n, obj);
@@ -85,97 +82,13 @@ public class FieldVisitor extends VoidVisitorAdapter<Object>  {
 		{
 			NodeList<VariableDeclarator> fieldlist = n.getVariables();
 			for(VariableDeclarator field: fieldlist)
-			{	
-				//String name = "";
-				String type = field.getType().toString();
-				AddRawFieldNameToList(field.toString());
-				AddVarToTypeEntry(field);
-				SetVisiblityForVar(n, field);
-				
-				if(type.contains("<"))
-				{
-					//System.out.println(type.substring(0,type.indexOf("<")));
-					if(collections.contains(type.substring(0,type.indexOf("<"))))
-					{
-						//System.out.println(type.substring(type.indexOf("<")+1,type.indexOf(">")));
-						String Ctype = type.substring(type.indexOf("<")+1,type.indexOf(">"));
-						if(Parser.ClassNames.contains(Ctype)||Parser.InterfaceNames.contains(Ctype)) {
-							boolean found = false;
-							//System.out.println("HERE");
-							for (ObjCount o : objCountList) {
-								if (o.className.equals(Ctype) && o.count < ObjCount.MAX_OBJ_COUNT) {
-									o.count += ObjCount.MAX_OBJ_COUNT;
-									found = true;
-									break;
-								}
-							}
-							if (!found) {
-								ObjCount o = new ObjCount(Ctype);
-								o.count = ObjCount.MAX_OBJ_COUNT;
-								objCountList.add(o);
-							}
-						}/**
-						else {
-							//VisiblityMod(n, field, name);
-						}*/
-					}
-				}
-
-				/**
-				else if(Parser.ClassNames.contains(type)==false && Parser.InterfaceNames.contains(type)==false)
-				{
-					//VisiblityMod(n, field, name);
-
-					//FieldName.add(name);
-					//System.out.println("field names : "+FieldName);
-				}*/
-
-				else if(Parser.ClassNames.contains(type) || Parser.InterfaceNames.contains(type))
-				{
-					boolean found = false;
-					for (Iterator<ObjCount> iterator = objCountList.iterator(); iterator.hasNext();) {
-						ObjCount o = iterator.next();
-						if (o.className.equals(type)) {
-							o.count++;
-							found = true;
-						}
-					}
-					if (!found) {
-						ObjCount o = new ObjCount(type);
-						o.count = 1;
-						objCountList.add(o);
-					}
-
-					//types.add(type);
-					//System.out.println("types : "+types);
-				}
+			{
+				AddVarToFieldAttrs(n, field);			
 			}
 		}
 	}
-	
-	public ArrayList<String> getFieldName(){
-		return FieldName;
-	}
 
-	public ArrayList<String> getFieldRawFieldNames(){
-		return rawFieldNames;
+	public ArrayList<FieldAttr> getFieldAttrs(){
+		return fieldAttrs;
 	}
-/**	
-	public ArrayList<String> getFieldTypes(){
-		return types;
-	}*/
-
-	// S
-	public ArrayList<ObjCount> getFieldObjCountList(){
-		return objCountList;
-	}
-
-	public HashMap<String, String> getFieldVarVisiblity(){
-		return varVisiblity;
-	}
-
-	public HashMap<String, String> getFieldVarToTypeMap(){
-		return varToTypeMap;
-	}
-
 }
